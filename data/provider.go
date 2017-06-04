da98ac300ffb5fd4d082971f828710aeb8e53f90
John DiStasio <jndistasio@gmail.com>
2017-06-04 12:22:32 -0400

    Add a solitary godoc

--- data/provider.go
+++ data/provider.go
@@ -2,6 +2,7 @@ package data
 
 type Data map[string]interface{}
 
+// A Provider is a generic source of data to use when populating a template.
 type Provider interface {
 	GetData() (Data, error)
 }
