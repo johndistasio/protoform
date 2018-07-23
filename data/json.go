fcf3ab4a96d5bd72b95dc19e92a0de6dfe28c6b9
John DiStasio <jndistasio@gmail.com>
2018-07-23 19:32:08 -0400

    Move JsonFile data source code into data/json.go

--- data/json.go
+++ data/json.go
@@ -4,14 +4,29 @@ import (
 	gojson "encoding/json"
 	"io"
 	"io/ioutil"
+	"os"
 )
 
 type Json struct {
 	reader io.Reader
 }
 
+type JsonFile struct {
+	*Json
+}
+
 func NewJson(r io.Reader) *Json {
 	return &Json{r}
+}
+
+func NewJsonFile(path string) (*JsonFile, error) {
+	file, err := os.OpenFile(path, os.O_RDONLY, 0600)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &JsonFile{&Json{file}}, nil
 }
 
 func (p *Json) GetData() (Data, error) {
